a9c0b123a48bbca56dedd58391d888e9dc86be7f
Sam Klepper <samklepdev@gmail.com>
2022-02-09 16:13:03 -0600

    Removed unused social icons & added crossOrigin="true" in Google Fonts cdn link.

--- components/Layout.js
+++ components/Layout.js
@@ -12,7 +12,11 @@ export default function Layout({ children, title, description, keywords }) {
         <meta name='description' content={description} />
         <link rel='icon' href='/favicon.ico' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
         <link
           href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap'
           rel='stylesheet'
