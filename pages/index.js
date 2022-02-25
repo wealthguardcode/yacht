27b69a8608963bf417a8b3e14bfac48c77b41f4f
Sam Klepper <samklepdev@gmail.com>
2022-02-25 12:04:52 -0600

    Home page-- deleted unused code in video carousel section.

--- pages/index.js
+++ pages/index.js
@@ -22,127 +22,21 @@ export default function Home() {
         className='carousel slide carousel-fade'
         data-mdb-ride='carousel'>
         {/* <!-- Indicators --> */}
-        <div className='carousel-indicators'>
-          {/* <button
-            type='button'
-            data-mdb-target='#carouselVideoExample'
-            data-mdb-slide-to='0'
-            className='active'
-            aria-current='true'
-            aria-label='Slide 1'></button> */}
-          {/* <button
-            type='button'
-            data-mdb-target='#carouselVideoExample'
-            data-mdb-slide-to='1'
-            aria-label='Slide 2'></button>
-          <button
-            type='button'
-            data-mdb-target='#carouselVideoExample'
-            data-mdb-slide-to='2'
-            aria-label='Slide 3'></button>
-          <button
-            type='button'
-            data-mdb-target='#carouselVideoExample'
-            data-mdb-slide-to='3'
-            aria-label='Slide 4'></button> */}
-        </div>
+        <div className='carousel-indicators'></div>
 
         {/* <!-- Inner --> */}
         <div className='carousel-inner carousel-wrapper'>
           {/* <!-- Single item --> */}
           <div className='carousel-item active' data-mdb-interval='36000'>
             <VideoPlayer />
-            {/* <video className='img-fluid' playsInline autoPlay loop muted>
-              <source
-                src='https://res.cloudinary.com/wealthguard-insurance-group/video/upload/v1644445555/yacht-video-1_x2dp0w.mp4'
-                type='video/mp4'
-              />
-            </video> */}
-            {/* <div className='carousel-caption d-none d-md-block'>
-              <h5>First slide label</h5>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div> */}
           </div>
-
-          {/* <!-- Single item --> */}
-          {/* <div className='carousel-item' data-mdb-interval='9500'>
-            <video className='img-fluid' autoPlay loop muted>
-              <source
-                src='/images/home/slider/videos/2-yacht-flyover-2.mp4'
-                type='video/mp4'
-              />
-            </video>
-            <div className='carousel-caption d-none d-md-block'>
-              <h5>Second slide label</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </div> */}
-
-          {/* <!-- Single item --> */}
-          {/* <div className='carousel-item' data-mdb-interval='9500'>
-            <video className='img-fluid' autoPlay loop muted>
-              <source
-                src='/images/home/slider/videos/3-steering-yacht.mp4'
-                type='video/mp4'
-              />
-            </video>
-            <div className='carousel-caption d-none d-md-block'>
-              <h5>Third slide label</h5>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </div>
-          </div> */}
-
-          {/* <!-- Single item --> */}
-          {/* <div className='carousel-item' data-mdb-interval='9500'>
-            <video className='img-fluid' autoPlay loop muted>
-              <source
-                src='/images/home/slider/videos/4-man-on-yacht.mp4'
-                type='video/mp4'
-              />
-            </video>
-            <div className='carousel-caption d-none d-md-block'>
-              <h5>Fourth slide label</h5>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </div>
-          </div> */}
         </div>
-        {/* <!-- Inner --> */}
-
-        {/* <!-- Controls --> */}
-        {/* <button
-          className='carousel-control-prev'
-          type='button'
-          data-mdb-target='#carouselVideoExample'
-          data-mdb-slide='prev'>
-          <span
-            className='carousel-control-prev-icon'
-            aria-hidden='true'></span>
-          <span className='visually-hidden'>Previous</span>
-        </button>
-        <button
-          className='carousel-control-next'
-          type='button'
-          data-mdb-target='#carouselVideoExample'
-          data-mdb-slide='next'>
-          <span
-            className='carousel-control-next-icon'
-            aria-hidden='true'></span>
-          <span className='visually-hidden'>Next</span>
-        </button> */}
       </div>
-      {/* <!-- Carousel wrapper --> */}
 
       {/* Main content */}
       <div className='bg-white'>
         <div className='max-w-7xl mx-auto py-8 md:py-16 px-4 lg:px-8'>
           <div className='text-left'>
-            {/* <h2 className='text-base font-semibold text-indigo-600 tracking-wide uppercase'>
-              Pricing
-            </h2> */}
             <p className='hidden md:block mt-1 text-4xl font-normal text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl'>
               There&apos;s a <span className='font-extrabold'>better way</span>{' '}
               to protect <br />
