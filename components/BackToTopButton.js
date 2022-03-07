a0018c38e77341f46a6ebd6b6f124075f4020e01
Sam Klepper <samklepdev@gmail.com>
2022-03-07 11:37:51 -0600

    Scroll to top wrapped inside useEffect hook to re-render.

--- components/BackToTopButton.js
+++ components/BackToTopButton.js
@@ -1,26 +1,29 @@
 import { ArrowUpIcon } from '@heroicons/react/solid'
+import { useEffect } from 'react'
 
 export default function BackToTopButton() {
-  if (typeof window !== 'undefined') {
-    //Get the button:
-    const mybutton = document.getElementById('myBtn')
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      //Get the button:
+      const mybutton = document.getElementById('myBtn')
 
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () {
-      scrollFunction()
-    }
+      // When the user scrolls down 20px from the top of the document, show the button
+      window.onscroll = function () {
+        scrollFunction()
+      }
 
-    function scrollFunction() {
-      if (
-        document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20
-      ) {
-        mybutton.style.display = 'block'
-      } else {
-        mybutton.style.display = 'none'
+      function scrollFunction() {
+        if (
+          document.body.scrollTop > 20 ||
+          document.documentElement.scrollTop > 20
+        ) {
+          mybutton.style.display = 'block'
+        } else {
+          mybutton.style.display = 'none'
+        }
       }
     }
-  }
+  })
 
   // When the user clicks on the button, scroll to the top of the document
   function scrollToTop() {
